c7a005146a2695d6a80892319a7542fc1dffce14
OKI <21fi019dendai@gmail.com>
2024-05-15 18:20:56 +0900

    html移動

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# 「始める」を押すと次のHTMLファイルに移動する
+@app.route("/start")
+def start():
+    # 移動先のHTMLファイルの名前を指定します
+    return render_template("/start_page.html")
+
+# 「続ける」を押すと次のHTMLファイルに移動する
+@app.route("/continue")
+def continue_game():
+    # 移動先のHTMLファイルの名前を指定します
+    return redirect("/continue_page.html")
 if __name__ == "__main__":
     app.run(debug=True)
+
