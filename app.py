1567ea26afcd9ff4b3f8643f9597ef72136363ae
21fi046 <21fi046@ms.dendai.ac.jp>
2024-05-16 00:33:01 +0900

    画面遷移

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def start():
 def continue_game():
     # 移動先のHTMLファイルの名前を指定します
     return redirect("/continue_page.html")
+
+
+@app.route("/first")
+def first():
+    # 移動先のHTMLファイルの名前を指定します
+    return render_template("/first_page.html")
+
+@app.route("/second")
+def second():
+    # 移動先のHTMLファイルの名前を指定します
+    return render_template("/second_page.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
