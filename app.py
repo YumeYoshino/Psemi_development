4a70c4e50b6f3535367195c4ce86611e392d889b
21fi046 <21fi046@ms.dendai.ac.jp>
2024-05-17 13:23:46 +0900

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("/index.html")
 
 # 「続ける」を押すと次のHTMLファイルに移動する
 @app.route("/continue")
