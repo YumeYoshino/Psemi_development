9c75951498ddd7e1bc47fd149619681d7264faed
21fi046 <21fi046@ms.dendai.ac.jp>
2024-05-17 12:50:25 +0900

    add

--- game.py
+++
@@ -1,8 +0,0 @@
-#今後実装予定
-
-import pygame as pg
-from draw import draw_text
-
-def game_main(screen, tmr, color):
-    screen.fill(color["black"])
-    draw_text(70, "始める", color["white"], screen, 30, 100)
