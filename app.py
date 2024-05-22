2c618d1ebfe1c4d3d7b293aedc4b45a8cee196a4
21fi046 <21fi046@ms.dendai.ac.jp>
2024-05-22 16:27:59 +0900

    question3

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("/index.html")
 
-# 「続ける」を押すと次のHTMLファイルに移動する
-@app.route("/question3")
-def question3():
-    # 移動先のHTMLファイルの名前を指定します
-    return render_template("/question3_page.html")
-
-
 @app.route("/first")
 def first():
     # 移動先のHTMLファイルの名前を指定します
@@ -63,11 +56,11 @@ def second_page_false():
     # 移動先のHTMLファイルの名前を指定します
     return render_template("/second_page_false.html")
 
-
-@app.route("/third")
-def third():
+# 「続ける」を押すと次のHTMLファイルに移動する
+@app.route("/question3")
+def question3():
     # 移動先のHTMLファイルの名前を指定します
-    return render_template("/third_page.html")
+    return render_template("/question3_page.html")
 
 @app.route("/Q3_correct")
 def correct3():
@@ -78,5 +71,11 @@ def correct3():
 def incorrect3():
     # 移動先のHTMLファイルの名前を指定します
     return render_template("/question3_incorrect.html")
+
+
+@app.route("/end")
+def end():
+    return render_template("/end.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
